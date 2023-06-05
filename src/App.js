641066adb8535251b9c7bad3413a115e195b5b68
tommygoX <tommyg@post.bgu.ac.il>
2023-06-05 15:59:53 +0300

    building first components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+import ParticlesBg from 'particles-bg';
+
 
 class App extends Component {
   render(){
     return (
      <div className="App">
-      
+      <ParticlesBg color="#ffffff" num={100} type="cobweb" bg={true} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* {<FaceRecognition />} */}
      </div>
     );
   }  
